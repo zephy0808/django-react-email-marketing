da6dca7331840de0c2b982c3228c8c3ce3104ee5
zephy0808 <eugenegarrett.info@gmail.com>
2025-04-13 11:16:31 -0300

    corr: backend w new app

--- backend/email_app/asgi.py
+++ backend/email_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for email_marketing project.
+ASGI config for email_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_marketing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_app.settings')
 
 application = get_asgi_application()
--- backend/email_app/urls.py
+++ backend/email_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for email_marketing project.
+URL configuration for email_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-,
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
