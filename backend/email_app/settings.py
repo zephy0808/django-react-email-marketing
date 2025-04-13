bd14759a5c4c860c477ba01e50e84c40fc4241e3
zephy0808 <eugenegarrett.info@gmail.com>
2025-04-13 14:24:32 -0300

    add: data and settings

--- backend/email_app/settings.py
+++ backend/email_app/settings.py
@@ -147,7 +147,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
